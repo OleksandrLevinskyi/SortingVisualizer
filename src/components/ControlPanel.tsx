183609edfee78abbe01136abc632abbe0cedc31e
OleksandrLevinskyi <aleksandrlevinskyi@gmail.com>
2023-01-29 00:05:32 -0500

    update range selectors

--- src/components/ControlPanel.tsx
+++ src/components/ControlPanel.tsx
@@ -1,52 +1,35 @@
 import type {Component} from 'solid-js';
-import Field from "./Field";
 import Dropdown from "./Dropdown";
 import TextTypeSelector from "./TextTypeSelector";
 import ColorModeSwitch from "./ColorModeSwitch";
 import Tutorial from "./Tutorial";
+import RangeSelector from "./RangeSelector";
 
 const ControlPanel: Component = () => {
     return (
         <div class="container-fluid">
-            <Field/>
+            <Dropdown/>
             <TextTypeSelector/>
+            <button type="button" class="btn btn-primary btn-block align-middle"
+                    id="generate">Generate<br/>New<br/>Array
+            </button>
+
+            <RangeSelector id={'count'}
+                           title={'Bar Count'}
+                           statId={'bar_count'}
+                           unit={'bars'}
+                           min={15}
+                           max={45}
+                           step={1}/>
+
+            <RangeSelector id={'delay'}
+                           title={'Animation Delay'}
+                           statId={'animation_delay'}
+                           unit={'ms'}
+                           min={5}
+                           max={500}
+                           step={5}/>
             <div class="row">
-                <Dropdown/>
-                <div class="col-md-6">
-                    <div class="row">
-                        <div class="col-md-2 not_padded">
-                            <button type="button" class="btn btn-primary btn-block align-middle"
-                                    id="generate">Generate<br/>New<br/>Array
-                            </button>
-                        </div>
-                        <div class="col-md-5 left_padded">
-                            <p class="heading">Bar Count <span class="data">(<span id="count"></span>
-                                bars)</span></p>
-                            <input type="range" min="10" max="45" step="1" id="bar_count"/>
-
-                            <p class="heading">Animation Delay <span class="data">(<span id="animation_delay"></span>
-                                ms)</span></p>
-                            <input type="range" min="5" max="500" step="5" id="delay"/>
-                        </div>
-                    </div>
-
-                    <hr/>
-
-                    <p class="text-center not_padded" id="description"><b>Choose Algorithm To Animate</b></p>
-                    <div class="row">
-                        <div class="col-md-6">
-                            <p class="text-center not_padded">
-                                <u>Comparisons: <span id="comparisons">N/A</span></u>
-                            </p>
-                        </div>
-                        <div class="col-md-6">
-                            <p class="text-center not_padded">
-                                <u>Array Manipulations: <span id="array_manipulations">N/A</span></u> <span
-                                class="data">(swap/change values)</span>
-                            </p>
-                        </div>
-                    </div>
-                </div>
                 <div class="col-md-3">
                     <p class="heading text-center">Create Custom Array</p>
                     <div class="row">
@@ -61,12 +44,6 @@ const ControlPanel: Component = () => {
                     <ColorModeSwitch/>
 
                     <Tutorial/>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-md-12">
-                    <hr/>
-                    <p class="text-center">&copy; 2021 Oleksandr Levinskyi - Sorting Visualizer</p>
                 </div>
             </div>
         </div>
