c4d03a4fcc9eaebcc86ccd78575142cd321ece0e
OleksandrLevinskyi <aleksandrlevinskyi@gmail.com>
2023-01-29 20:43:26 -0500

    fix QuickSort.ts

--- src/ts/algos/sorts/QuickSort.ts
+++ src/ts/algos/sorts/QuickSort.ts
@@ -1,57 +1,70 @@
 import {ISortStrategy} from "../ISortStrategy";
-import {pause} from "../../utils/utils";
+import {getRectValue, pause} from "../../utils/utils";
 import {Context} from "../../Context";
+import {swap} from "../../utils/sort_utils";
+import {updateNumbers} from "../../utils/panel_utils";
 
 class QuickSort implements ISortStrategy {
     async sort(arr: Array<any>, start = 0, end = arr.length - 1): Promise<any> {
         const context: Context = Context.getContext();
-        if (start >= end) return arr;
 
-        let pivotIdx = await placePivot(arr, start, end);
-        await quickSort(arr, start, pivotIdx - 1);
-        await quickSort(arr, pivotIdx + 1, end);
+        if (start >= end) {
+            return arr;
+        }
+
+        let pivotIdx = await this.placePivot(arr, start, end);
+
+        await this.sort(arr, start, pivotIdx - 1);
+        await this.sort(arr, pivotIdx + 1, end);
 
         if (start == 0 && end == arr.length - 1) {
             arr.forEach(r => r.classList = 'sorted');
-            await pause(speed);
+            await pause(context.speed);
             arr.forEach(r => r.classList = '');
         }
     }
 
-    async placePivot(arr, start = 0, end = arr.length - 1) {
+    async placePivot(arr: Array<any>, start: number = 0, end: number = arr.length - 1): Promise<number> {
+        const context: Context = Context.getContext();
+        let pivotIdx: number = start;
+        let prev: any;
+
         arr[start].classList.add('edge');
         arr[end].classList.add('edge');
 
-        let pivotIdx = start, prev;
         for (let i = start + 1; i <= end; i++) {
-            await pause(speed);
+            await pause(context.speed);
+
             arr[i].classList.add('curr');
-            if (prev != null) prev.classList.remove('curr');
+            if (prev != null) {
+                prev.classList.remove('curr');
+            }
+
             prev = arr[i];
+
             if (getRectValue(arr[start]) > getRectValue(arr[i])) {
                 pivotIdx++;
-                // await pause(speed);
-                // arr[i].classList.add('pivot');
-                // arr[pivotIdx].classList.remove('pivot');
                 await swap(arr, pivotIdx, i, false);
             }
 
-            comparisons++;
+            context.comparisons++;
             updateNumbers();
         }
-        await pause(speed);
+
+        await pause(context.speed);
         arr[start].classList.remove('edge');
         arr[end].classList.remove('edge');
         arr[end].classList.remove('curr');
 
-        await pause(speed);
+        await pause(context.speed);
         arr[start].classList.add('pivot');
         arr[pivotIdx].classList.add('pivot');
         await swap(arr, start, pivotIdx, false);
 
-        await pause(speed);
+        await pause(context.speed);
         arr[start].classList.remove('pivot');
         arr[pivotIdx].classList = 'sorted';
+
         return pivotIdx;
     }
 }
