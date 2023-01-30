6676e94de7222ba785ea29e12febb3f5e8cfe412
OleksandrLevinskyi <aleksandrlevinskyi@gmail.com>
2023-01-29 20:49:18 -0500

    fix MergeSort.ts

--- src/ts/algos/sorts/MergeSort.ts
+++ src/ts/algos/sorts/MergeSort.ts
@@ -1,7 +1,7 @@
 import {ISortStrategy} from "../ISortStrategy";
-import {pause} from "../../utils/utils";
+import {getRectValue, pause} from "../../utils/utils";
 import {Context} from "../../Context";
-import {sort} from "../../utils/panel_utils";
+import {updateNumbers} from "../../utils/panel_utils";
 
 class MergeSort implements ISortStrategy {
     async sort(arr: Array<any>, startIdx = 0): Promise<Array<any>> {
@@ -18,30 +18,32 @@ class MergeSort implements ISortStrategy {
         return await this.merge(left, right, startIdx);
     }
 
-    async merge(arr1, arr2, startIdx) {
-        let arr = [], vals = [],
-            prev = null,
-            i = 0, j = 0,
-            middleIdx = startIdx + arr1.length,
-            endIdx = startIdx + arr1.length + arr2.length - 1;
+    async merge(arr1: Array<any>, arr2: Array<any>, startIdx: number): Promise<Array<any>> {
+        const context: Context = Context.getContext();
 
-        await pause(speed);
-        currArr[startIdx].classList = 'sorted'; // select the current one
-        currArr[middleIdx].classList = 'curr'; // select the current one
-        currArr[endIdx].classList = 'sorted'; // select the current one
+        let arr: Array<any> = [];
+        let vals: Array<number> = [];
+        let prev: any = null;
+        let i: number = 0;
+        let j: number = 0;
+        let middleIdx: number = startIdx + arr1.length;
+        let endIdx: number = startIdx + arr1.length + arr2.length - 1;
 
+        await pause(context.speed);
+        context.currArr[startIdx].classList = 'sorted'; // select the current one
+        context.currArr[middleIdx].classList = 'curr'; // select the current one
+        context.currArr[endIdx].classList = 'sorted'; // select the current one
 
         while (i < arr1.length && j < arr2.length) {
-            await pause(speed);
-            if (prev != null) resetColorMergeSort(prev, currArr, startIdx, endIdx, middleIdx); // de-select old one
+            await pause(context.speed);
+            if (prev != null) this.resetColorMergeSort(prev, context.currArr, startIdx, endIdx, middleIdx); // de-select old one
             if (getRectValue(arr1[i]) <= getRectValue(arr2[j])) {
                 arr1[i].classList = 'next'; // select the current one
                 arr.push(arr1[i]);
 
                 prev = arr1[i];
                 i++;
-            }
-            else {
+            } else {
                 arr2[j].classList = 'next'; // select the current one
                 arr.push(arr2[j]);
 
@@ -49,14 +51,15 @@ class MergeSort implements ISortStrategy {
                 j++;
             }
 
-            comparisons++;
+            context.comparisons++;
             updateNumbers();
         }
 
         while (i < arr1.length) {
-            await pause(speed);
-            if (prev != null) resetColorMergeSort(prev, currArr, startIdx, endIdx, middleIdx); // de-select old one
-            // arr1[i == 0 ? i : i - 1].classList = ''; // de-select old one
+            await pause(context.speed);
+            if (prev != null) {
+                this.resetColorMergeSort(prev, context.currArr, startIdx, endIdx, middleIdx); // de-select old one
+            }
             arr1[i].classList = 'next'; // select the current one
             arr.push(arr1[i]);
 
@@ -65,8 +68,8 @@ class MergeSort implements ISortStrategy {
         }
 
         while (j < arr2.length) {
-            await pause(speed);
-            if (prev != null) resetColorMergeSort(prev, currArr, startIdx, endIdx, middleIdx); // de-select old one
+            await pause(context.speed);
+            if (prev != null) this.resetColorMergeSort(prev, context.currArr, startIdx, endIdx, middleIdx); // de-select old one
             // arr2[j == 0 ? j : j - 1].classList = ''; // de-select old one
             arr2[j].classList = 'next'; // select the current one
             arr.push(arr2[j]);
@@ -78,42 +81,39 @@ class MergeSort implements ISortStrategy {
         arr.forEach(e => vals.push(getRectValue(e)));
 
         for (let a = startIdx; a < arr.length + startIdx; a++) {
-            await pause(speed);
+            await pause(context.speed);
             let idx = a - startIdx;
 
-            if (prev != null) resetColorMergeSort(prev, currArr, startIdx, endIdx, middleIdx); // de-select old one
+            if (prev != null) this.resetColorMergeSort(prev, context.currArr, startIdx, endIdx, middleIdx); // de-select old one
 
-            let newHeight = yScale(vals[idx]);
-            currArr[a].setAttribute('height', newHeight);
-            currArr[a].setAttribute('y', height - newHeight);
-            currArr[a].setAttribute('val', vals[idx]);
-            currArr[a].parentNode.querySelector('text').innerHTML = vals[idx];
-            arr[idx] = currArr[a];
-            currArr[a].classList = 'next'; // select the current one
+            let newHeight = context.yScale(vals[idx]);
+            context.currArr[a].setAttribute('height', newHeight);
+            context.currArr[a].setAttribute('y', context.height - newHeight);
+            context.currArr[a].setAttribute('val', vals[idx]);
+            context.currArr[a].parentNode.querySelector('text').innerHTML = vals[idx];
+            arr[idx] = context.currArr[a];
+            context.currArr[a].classList = 'next'; // select the current one
 
-            prev = currArr[a];
+            prev = context.currArr[a];
 
-            arrayAccesses++;
+            context.arrayAccesses++;
             updateNumbers();
         }
 
-        await pause(speed);
-        currArr[startIdx].classList = ''; // select the current one
-        currArr[middleIdx].classList = ''; // select the current one
-        currArr[endIdx].classList = ''; // select the current one
-
+        await pause(context.speed);
+        context.currArr[startIdx].classList = ''; // select the current one
+        context.currArr[middleIdx].classList = ''; // select the current one
+        context.currArr[endIdx].classList = ''; // select the current one
 
         return arr;
     }
 
-    resetColorMergeSort(rect, currArr, startIdx, endIdx, middleIdx) {
+    resetColorMergeSort(rect: any, currArr: Array<any>, startIdx: number, endIdx: number, middleIdx: number): void {
         if (rect == currArr[startIdx] || rect == currArr[endIdx]) {
             rect.classList = 'sorted';
-        }
-        else if (rect == currArr[middleIdx]) {
+        } else if (rect == currArr[middleIdx]) {
             rect.classList = 'curr';
-        }
-        else {
+        } else {
             rect.classList = '';
         }
     }
